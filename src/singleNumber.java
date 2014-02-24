09e13b7041557886313fadac43fec1b4843add79
pbwpbw2008 <pbwpbw2008@yahoo.com>
2014-02-24 13:11:08 -0500

    1st Commit

---
+++ src/singleNumber.java
@@ -0,0 +1,35 @@
+/*
+ * Questions: Given an array of integers, every element appears twice except for one. Find that single one.
+ * 
+ * Solutions: Use the Java "^" operations
+ * 
+ * Details:
+ * 
+ * 0 ^ n = n; n ^ n = 0;
+ * 
+ *  0100:4
+ *  0110:6  (^)
+ *  0010:2
+ * 
+ * 4 ^ 6 = 2
+ * 
+ * a^b^c = a^c^b
+ * thus two interact with each other, only left the single one
+ * 
+
+*/
+
+public class singleNumber {
+
+	public static int singleNumber(int[] A){
+		int XOR = 0;
+		for(int i = 0; i< A.length; i++)
+			XOR ^= A[i];
+		return XOR;
+	}
+	
+	public static void main(String[] args){
+		int[] A = new int[]{1,2,3,4,5,5,4,3,2,1,100};
+		System.out.println("The single value is " + singleNumber(A));
+	}
+}
